5fb4dfdd59c48603337b6ad68fe367396c39382c
Umar Farid <umarfarid1407@gmail.com>
2025-01-02 18:49:42 +0500

    endintern

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,32 +3,31 @@ import SignIn from "./components/user/sign-in/SignIn";
 import SignUp from "./components/user/sign-up/SignUp";
 import Profile from "./components/user/Dashboards/User/profile/profile";
 import GetAllProducts from "./components/user/Dashboards/User/Products/getAllProducts";
-import PastPurchasing   from "./components/user/Dashboards/User/Products/pastPurchasing"
+import PastPurchasing from "./components/user/Dashboards/User/Products/pastPurchasing";
 import ThrowUser from "./components/user/Dashboards/throwUser";
-import { AdminNavbar, SellerNavbar,PharmacistNavbar,UserDashboard } from "./sharedimports/share";
-import UploadPhoto from "./components/user/uploadfile";
+import {
+  AdminNavbar,
+  SellerNavbar,
+  PharmacistNavbar,
+  UserDashboard,
+} from "./sharedimports/share";
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <SignUp disableCustomTheme={true} /> */}
-
-
-        {/* <Route path="/" element={<UploadPhoto />} /> */}
         <Route path="/" element={<UserDashboard />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
 
-
-        <Route path= "/admin" element = {<AdminNavbar/>} />
-        <Route path= "/seller" element = {<SellerNavbar/>} />
-        <Route path= "/pharmacist" element = {<PharmacistNavbar/>} />
+        <Route path="/admin" element={<AdminNavbar />} />
+        <Route path="/seller" element={<SellerNavbar />} />
+        <Route path="/pharmacist" element={<PharmacistNavbar />} />
 
         <Route path="/throwuser" element={<ThrowUser />} />
 
         <Route path="/profile" element={<Profile />} />
         <Route path="/products" element={<GetAllProducts />} />
-        <Route path= "/pastpurchasing" element= {<PastPurchasing/>}/>
+        <Route path="/pastpurchasing" element={<PastPurchasing />} />
       </Routes>
     </Router>
   );
